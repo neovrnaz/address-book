9a1d1bf5b2b141df041bb6994f4f9da7e43d8e48
neovrnaz <neovrnaz@gmail.com>
2020-10-17 22:13:25 -0600

    Add ability to search by phone number

--- address_book.rb
+++ address_book.rb
@@ -37,6 +37,26 @@ class AddressBook
       puts "\n"
     end
   end
+
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub('-', '')
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        results.push(contact) if phone_number.number.gsub('-', '').include?(search)
+      end
+    end
+    puts "Name search results that include \"#{search}\"."
+    puts "-" * 30
+    puts results
+    puts "-" * 30
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
 end
 
 address_book = AddressBook.new
@@ -44,8 +64,8 @@ richard = Contact.new
 richard.first_name = 'Richard'
 richard.middle_name = 'Guy'
 richard.last_name = 'Tea'
-richard.add_phone_number('Home', '(123)456-7890')
-richard.add_phone_number('Cell', '(123)456-7890')
+richard.add_phone_number('Home', '202-555-0180')
+richard.add_phone_number('Cell', '202-555-0193')
 richard.add_birthday('October 3')
 richard.add_address('Home', '123 Main St.', '', 'Portland', 'OR', '12345')
 
@@ -53,7 +73,7 @@ russel = Contact.new
 russel.first_name = 'Russel'
 russel.middle_name = ''
 russel.last_name = 'Sprout'
-russel.add_phone_number('Cell', '(123)456-7890')
+russel.add_phone_number('Cell', '202-555-0750')
 russel.add_birthday('March 21')
 russel.add_address('Work', '4908  Moonlight Drive.', '', 'Palermo', 'NJ', '08230')
 
@@ -62,4 +82,5 @@ address_book.contacts.push(russel)
 
 # address_book.print_contact_list
 
-address_book.find_by_name('richard')
+# address_book.find_by_name('richard')
+address_book.find_by_phone_number('1')
